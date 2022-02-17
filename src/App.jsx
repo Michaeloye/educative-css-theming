c9638564a6f4dfd7cc51709fdf6adad1626af365
michaeloye <michaeloye23@gmail.com>
2022-02-17 16:32:07 +0100

    Add initializeTheming

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import "./App.css";
 import { initializeTheming } from "css-theming";
 
 function App() {
+  // Initializes the main default category
+  initializeTheming();
+
+  // Initializes the sidebar category
+  initializeTheming(/* theme */ null, /* category */ "sidebar");
   return (
     <div className="App">
       <header className="App-header"></header>
